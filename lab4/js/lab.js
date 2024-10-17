6f7bf6cc9529beab0c08bef43c836aa75bfd794f
emilyswem <eswem@ucsc.edu>
2024-10-17 15:05:36 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Connect 4
+//An implementation of the game
+// Authors: Emily Swem and Cristalyn Murray
+// Date: October 2024
+
+  //Print a board of blank dots 6 height, 7 width
+    //Accept user input along top row
+      //Do not allow moves into occupied spaces and columns
+    //Validate user move
+      //Move user chip to lowest unoccupied space in column
+    //Choose computer move
+  //Test win or draw condition
+
 
 // Constants
 
