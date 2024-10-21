8fa7a0b8dff027fa0a27194b6db797250da92785
emilyswem <eswem@ucsc.edu>
2024-10-21 14:52:42 -0700

    bug fix

---
+++ lab5/js/lab5.js
@@ -0,0 +1,37 @@
+// index.js - purpose and description here
+// Author: Emily Swem and Cristalyn Murray
+// Date: 10/21/2024
+
+// Define Variables
+make = "Subaru";
+model = "Forester";
+color = "silver";
+year = "2018";
+
+// calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
