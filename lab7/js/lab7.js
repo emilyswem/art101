588f8f25731e96d463c17af49c4633a12a606e8f
emilyswem <eswem@ucsc.edu>
2024-10-29 17:43:18 -0700

    lecture

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -1,22 +1,14 @@
-// index.js - create mixed up names
+// create mixed up names
 // Author: Emily Swem and Cristalyn Murray
 // Date: 10/28/24
 
-//sortUserName - a funtion that takes user input and sorts the letters of their name
-function sortUserName () {
-    var userName = window.propmt ("Hey diva! Gimme ur name and I'll give you something new");
-    console.log ("userName=", userName);
-    //split the string to array
-    var nameArray = user.Name.split('');
-    console.log ("nameArray =", nameArray);
-    //sort the array
-    var nameArraySort = nameArray.sort ();
-    //join array back to a string
-    var nameSorted = nameArraySort.join ('');
-    console.log ("nameSorted=", nameSorted);
-    return nameSorted;
+
+//get string from user, sort a string, return a sorted string
+function sortStr() {
+    let userName = window.prompt ("hey diva! what's ur name?")
+    return userName("").sort().join("");
 }
 
-//output
-document.writeln("Hey girlie, new nickname:")
-    sortUserName(), "</br";
+
+//call the function and print results to html doc
+document.writeln(sortStr());
