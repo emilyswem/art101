3d71556c765b8340a3e0259021620c56918a2cdb
emilyswem <eswem@ucsc.edu>
2024-11-04 15:25:04 -0800

    lab 9

---
+++ lab 9/js/lab9.js
@@ -0,0 +1,22 @@
+// lab 9
+// Author: Emily Swem and Cristalyn Murray
+// Date: 11/4/24
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+// add button to problems section
+$("#problems").append("<button id='button-challenge'>Make Special</button>");
+// add button to results section
+$("#results").append("<button id='button-challenge'>Make Special</button>");
+// add a click listener to the challenge button
+$("button-challenge").click(function(){
+    $("#challenge").toggleClass("special");
+});
+// add a click listener to the problems button
+$("button-problems").click(function(){
+    $("#problems").toggleClass("special");
+});
+// add a click listener to the results button
+$("button-results").click(function(){
+    $("#results").toggleClass("special");
+});
