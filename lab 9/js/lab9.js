4a83a6f9604aae1bbbe8399008cd0d9af70670eb
emilyswem <eswem@ucsc.edu>
2024-11-04 15:36:13 -0800

    update to class

--- lab 9/js/lab9.js
+++ lab 9/js/lab9.js
@@ -3,20 +3,20 @@
 // Date: 11/4/24
 
 // add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$(".challenge").append("<button id='button-challenge'>Make Special</button>");
 // add button to problems section
-$("#problems").append("<button id='button-challenge'>Make Special</button>");
+$(".problems").append("<button id='button-challenge'>Make Special</button>");
 // add button to results section
-$("#results").append("<button id='button-challenge'>Make Special</button>");
+$(".results").append("<button id='button-challenge'>Make Special</button>");
 // add a click listener to the challenge button
 $("button-challenge").click(function(){
-    $("#challenge").toggleClass("special");
+    $(".challenge").toggleClass("special");
 });
 // add a click listener to the problems button
 $("button-problems").click(function(){
-    $("#problems").toggleClass("special");
+    $(".problems").toggleClass("special");
 });
 // add a click listener to the results button
 $("button-results").click(function(){
-    $("#results").toggleClass("special");
+    $(".results").toggleClass("special");
 });
