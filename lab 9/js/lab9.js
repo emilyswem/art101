324803a661c80f989a35ccbdf3d833592445aaaf
emilyswem <eswem@ucsc.edu>
2024-11-05 17:38:25 -0800

    fix in lecture

--- lab 9/js/lab9.js
+++ lab 9/js/lab9.js
@@ -3,20 +3,22 @@
 // Date: 11/4/24
 
 // add button to challenge section
-$(".challenge").append("<button id='button-challenge'>Make Special</button>");
-// add button to problems section
-$(".problems").append("<button id='button-challenge'>Make Special</button>");
-// add button to results section
-$(".results").append("<button id='button-challenge'>Make Special</button>");
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
 // add a click listener to the challenge button
-$("button-challenge").click(function(){
-    $(".challenge").toggleClass("special");
+$("#button-challenge").click(function(){
+    $("#challenge").toggleClass("special");
 });
+
+// add button to problems section
+$("#problems").append("<button id='button-challenge'>Make Special</button>");
 // add a click listener to the problems button
-$("button-problems").click(function(){
-    $(".problems").toggleClass("special");
+$("#button-problems").click(function(){
+    $("#problems").toggleClass("special");
 });
+
+// add button to results section
+$("#results").append("<button id='button-challenge'>Make Special</button>");
 // add a click listener to the results button
-$("button-results").click(function(){
-    $(".results").toggleClass("special");
+$("#button-results").click(function(){
+    $("#results").toggleClass("special");
 });
