3fb3b2def8423316f90d2b5ff9bdc7252aaf8cb4
emilyswem <eswem@ucsc.edu>
2024-11-06 16:50:08 -0800

    credit/fancy

--- lab 10/js/lab10.js
+++ lab 10/js/lab10.js
@@ -1,4 +1,4 @@
-// lab 9
+// lab 9 - Art 101 with Wes Modes
 // Author: Emily Swem and Cristalyn Murray
 // Date: 11/4/24
 
@@ -6,7 +6,7 @@
 function generateRandomText() {
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     const min = 3;
-    const max = 100;
+    const max = 200;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
     // Get a random starting index to slice the Lorem Ipsum text
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
@@ -15,9 +15,10 @@ function generateRandomText() {
   }
 
 // click listener for button
-    $("#make-convo").click(function(){
-            // get new fake dialogue
-            const newText = generateRandomText();
-            // append a new div to our output div
-            $("#output").append('<div class="text"><p>' + newText + '</p></div>');
-    });
+$("#make-convo").click(function(){
+    // get new fake dialogue
+    const newText = generateRandomText();
+  
+    // append a new div to our output div
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+  });
