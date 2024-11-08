742debd5fd6ab74fe8724312c2b1df3692ee5975
emilyswem <eswem@ucsc.edu>
2024-11-07 18:19:27 -0800

    lab 11

---
+++ lab 11/js/lab11.js
@@ -0,0 +1,18 @@
+// lab 9 - Art 101 with Wes Modes
+// Author: Emily Swem and Cristalyn Murray
+// Date: 11/7/24
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+    // We have to convert our string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+  }
+// click listener for button
+$("#submit").click(function(){
+// get value of input field
+const userName = $("#user-name").val();
+// now let's sort it
+userNameSorted = sortString(userName);
+// append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
